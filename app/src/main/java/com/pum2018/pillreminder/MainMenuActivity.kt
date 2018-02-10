6a3c2746158129b0835fe8182fdb46fb977d9c0c
robmik1974 <ronert1674@gmail.com>
2018-02-10 22:42:50 +0100

    adding new layout

--- app/src/main/java/com/pum2018/pillreminder/MainMenuActivity.kt
+++ app/src/main/java/com/pum2018/pillreminder/MainMenuActivity.kt
@@ -1,12 +1,37 @@
 package com.pum2018.pillreminder
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 
 class MainMenuActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+    }
+
+
+
+    fun onStoreClick(view : View){
+        Toast.makeText(this,"You pressed Store",LENGTH_SHORT).show()
+    }
+
+    fun onSettingsClick(view : View){
+        Toast.makeText(this,"You pressed Settings", LENGTH_SHORT).show()
+    }
+
+    fun onReportClick(view : View){
+        Toast.makeText(this,"You pressed Report", LENGTH_SHORT).show()
+    }
+
+    fun onScheduleClick(view : View){
+        Toast.makeText(this,"You pressed Schedule", LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/pum2018/pillreminder/ScheduleActivity.kt
@@ -0,0 +1,12 @@
+package com.pum2018.pillreminder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule)
+    }
+}

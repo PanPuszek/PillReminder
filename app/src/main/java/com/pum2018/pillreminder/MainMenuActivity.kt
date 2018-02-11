40934a82bcf9f6fea79b73298655b5697df23f4a
robmik1974 <ronert1674@gmail.com>
2018-02-11 16:39:30 +0100

    completed MainMenu

--- app/src/main/java/com/pum2018/pillreminder/MainMenuActivity.kt
+++ app/src/main/java/com/pum2018/pillreminder/MainMenuActivity.kt
@@ -1,11 +1,14 @@
 package com.pum2018.pillreminder
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
 import android.view.View
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import kotlinx.android.synthetic.main.activity_main_menu.*
 
 class MainMenuActivity : AppCompatActivity() {
 
@@ -15,6 +18,12 @@ class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+
+        val bitMap = BitmapFactory.decodeResource(resources, R.drawable.ic_alarm_add_black_48dp)
+        val rounded = RoundedBitmapDrawableFactory.create(resources,bitMap)
+        rounded.cornerRadius = 30f
+        imageScheduler.setImageDrawable(rounded)
     }
 
 
